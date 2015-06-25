8d6d411a728fb26074b0b36f792faae7bc0108e1
chinakids <494063570@qq.com>
2015-06-25 12:42:24 +0800

    初始化文件

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	require('time-grunt')(grunt);
+
+  grunt.initConfig({
+	  pkg: grunt.file.readJSON('package.json'),
+	  watch: {
+		  less: {
+		    files: [
+		        'less/**/*.less'
+		    ],
+		    tasks: ['less']
+		  }
+		},
+		less: {
+      fontIco: {
+        options: {
+          banner: '/*\n * <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd HH:MM") %>)\n * <%= pkg.description %>\n * Author:<%= pkg.author %>\n */\n',
+          compress: false,
+          sourceMap:false
+        },
+        files: {
+          "css/frontIco.css": "less/frontIco.less",
+        }
+      },
+      compress:{
+        options: {
+          banner: '/*\n * <%= pkg.name %> - Compress - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd HH:MM") %>)\n * <%= pkg.description %>\n * Author:<%= pkg.author %>\n */\n',
+          compress: true,
+          sourceMap:true
+        },
+        files: {
+          "dest/frontIco.min.css": "less/frontIco.less",
+        }
+      }
+    }
+	});
+
+    // 注冊任务
+    grunt.registerTask('default', [
+    	'less',
+      'watch'
+  	]);
+    grunt.event.on('watch', function(action, filepath, target) {
+	  grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+	});
+};
